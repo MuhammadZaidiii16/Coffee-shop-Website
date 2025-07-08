d0fd68cef565c8a5c18d13c7113b362a3b92cdf4
MuhammadZaidiii16 <naeemzaid51@gmail.com>
2025-07-08 10:10:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  //toogle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+//close menu
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+//close menu
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+//swiper initialize 
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //responsive breakoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
